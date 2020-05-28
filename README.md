f65665a721a2602229ff3382a62aa8b6d13da579
Hidetake Iwata <int128@gmail.com>
2020-05-28 20:31:10 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # hello-ec2-cloud-init
-Hello World of EC2 cloud-init
+
+This is Hello World of Amazon EC2 with cloud-init.
+
+
+## Getting Started
+
+```sh
+brew install terraform
+brew cask install session-manager-plugin
+```
+
+```sh
+# provision the resources
+terraform init
+terraform apply
+```
+
+This example will launch an instance running a background service.
+See [userdata.yaml](userdata.yaml) for more.
+
+```console
+% aws ssm start-session --target i-0935e9832a6391309 --region us-west-2
+
+$ tail -f /tmp/helloworld.log
+Thu May 28 11:29:08 UTC 2020
+Thu May 28 11:29:09 UTC 2020
+Thu May 28 11:29:10 UTC 2020
+...
+```
---
+++ main.tf
@@ -0,0 +1,64 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+# AMI of the latest Amazon Linux 2 
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "block-device-mapping.volume-type"
+    values = ["gp2"]
+  }
+}
+
+resource "aws_instance" "this" {
+  ami                  = data.aws_ami.amazon_linux_2.id
+  instance_type        = "t3.micro"
+  iam_instance_profile = aws_iam_instance_profile.this.name
+
+  # https://cloudinit.readthedocs.io/en/latest/topics/modules.html
+  user_data_base64 = filebase64("./userdata.yaml")
+}
+
+resource "aws_iam_instance_profile" "this" {
+  role = aws_iam_role.this.name
+}
+
+resource "aws_iam_role" "this" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.this.name
+}
