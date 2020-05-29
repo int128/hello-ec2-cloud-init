bc9a304be473c099d61ecd513b0e3f7ff14c2194
Hidetake Iwata <int128@gmail.com>
2020-05-29 18:17:19 +0900

    Refactor

--- main.tf
+++ main.tf
@@ -2,32 +2,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-# AMI of the latest Amazon Linux 2 
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  filter {
-    name   = "block-device-mapping.volume-type"
-    values = ["gp2"]
-  }
-}
-
 resource "aws_instance" "this" {
   ami                  = data.aws_ami.amazon_linux_2.id
   instance_type        = "t3.micro"
@@ -53,6 +27,32 @@ resource "aws_instance" "this" {
       ],
     })
   ]))
+}
+
+# AMI of the latest Amazon Linux 2 
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "block-device-mapping.volume-type"
+    values = ["gp2"]
+  }
 }
 
 resource "aws_iam_instance_profile" "this" {
